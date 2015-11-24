919d3a36db3b15ae5f0a78a88b4782cf7eea8cf1
Maricano <kenneth_fallesen@hotmail.com>
2015-11-24 13:35:51 +0100

    Context rettet

--- VOLLEYTEAMEsbjergServer/DataAccessLayer/Context/Context.cs
+++ VOLLEYTEAMEsbjergServer/DataAccessLayer/Context/Context.cs
@@ -22,30 +22,39 @@ namespace DataAccessLayer.Context
         {
             base.OnModelCreating(modelbuilder);
             var newsConfig = modelbuilder.Entity<DomainModelNews>();
+            newsConfig.Property(domainModelNews => domainModelNews.News).HasColumnType("NVARCHAR").HasMaxLength(2100);
             newsConfig.ToTable("News");
 
             base.OnModelCreating(modelbuilder);
             var aboutConfig = modelbuilder.Entity<DomainModelAbout>();
+            aboutConfig.Property(domainModelAbout => domainModelAbout.About).HasColumnType("NVARCHAR").HasMaxLength(2100);
             aboutConfig.ToTable("About");
 
             base.OnModelCreating(modelbuilder);
             var boardConfig = modelbuilder.Entity<DomainModelBoard>();
+            boardConfig.Property(domainModelBoard => domainModelBoard.Board).HasColumnType("NVARCHAR").HasMaxLength(2100);
             boardConfig.ToTable("Board");
 
             base.OnModelCreating(modelbuilder);
             var contactsConfig = modelbuilder.Entity<DomainModelContacts>();
+            contactsConfig.Property(domainModelContacts => domainModelContacts.Conteacts).HasColumnType("NVARCHAR").HasMaxLength(2100);
             contactsConfig.ToTable("Contacts");
 
             base.OnModelCreating(modelbuilder);
             var sponserConfig = modelbuilder.Entity<DomainModelSponser>();
+            sponserConfig.Property(domainModelSponser => domainModelSponser.Sponser).HasColumnType("NVARCHAR").HasMaxLength(2100);
             sponserConfig.ToTable("Sponser");
 
             base.OnModelCreating(modelbuilder);
             var turnamentConfig = modelbuilder.Entity<DomainModelTurnament>();
+            turnamentConfig.Property(domainModelTurnament => domainModelTurnament.Turnament)
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(2100);
             turnamentConfig.ToTable("Turnament");
 
             base.OnModelCreating(modelbuilder);
             var teamConfig = modelbuilder.Entity<DomainModelTeam>();
+            teamConfig.Property(domainModelTeam => domainModelTeam.Team).HasColumnType("NVARCHAR").HasMaxLength(2100);
             teamConfig.ToTable("Team");
         }
 
