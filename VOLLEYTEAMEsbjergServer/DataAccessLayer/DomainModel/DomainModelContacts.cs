a0a589e004fd93f494a139b8ea78c5520542e370
Maricano <kenneth_fallesen@hotmail.com>
2015-11-24 10:21:11 +0100

    domainModel

---
+++ VOLLEYTEAMEsbjergServer/DataAccessLayer/DomainModel/DomainModelContacts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.DomainModel
+{
+   public class DomainModelContacts
+    {
+       public int Id { get; set; }
+       public String Conteacts { get; set; }
+
+    }
+}
---
+++ VOLLEYTEAMEsbjergServer/DataAccessLayer/DomainModel/DomainModelNews.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.DomainModel
+{
+   public  class DomainModelNews
+    {
+       public int Id{ get; set; }
+       public String News { get; set; }    
+    }
+}
