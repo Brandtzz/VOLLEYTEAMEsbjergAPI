c0a41c66b1052105edc95a90e62f5bd073462220
FrederikSorensen <fred5277@easv365.dk>
2015-11-25 10:05:42 +0100

    Lavet DB initializer samt volleyteamDbInitializer

---
+++ VOLLEYTEAMEsbjergServer/DataAccessLayer/Context/DbInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Context
+{
+    public class DbInitializer
+    {
+        public static void Initialize()
+        {
+            Database.SetInitializer(new VolleyteamDbInitializer());
+        }
+    }
+}
