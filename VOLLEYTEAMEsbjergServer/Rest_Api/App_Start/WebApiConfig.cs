deb8e0c8b34564343c2c19b7b5fd41ed952a58e8
Maricano <kenneth_fallesen@hotmail.com>
2015-12-11 11:42:14 +0100

    WORKS

--- VOLLEYTEAMEsbjergServer/Rest_Api/App_Start/WebApiConfig.cs
+++ VOLLEYTEAMEsbjergServer/Rest_Api/App_Start/WebApiConfig.cs
@@ -22,7 +22,7 @@ namespace Rest_Api
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{Id}",
+                routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
